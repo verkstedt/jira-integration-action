f843b6fa1d151cad9140be3a7e197775b8bba9bc
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2024-09-06 17:51:32 +0200

    Nicer PR link

--- index.mjs
+++ index.mjs
@@ -204,7 +204,9 @@ async function assignPrToIssues(issueIds, pr) {
 
       const prLinkObject = {
         url: pr.html_url,
-        title: `PR #${pr.number}: ${pr.title}`,
+        // Using URL as title will make JIRA fetch the title itself
+        title: pr.html_url,
+        icon: { url16x16: 'https://github.com/favicon.ico' },
       }
 
       const { data: links } = await jiraApi.get(
